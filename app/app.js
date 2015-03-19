8e7e125bfa67fa860c5bfda73c0ade89a1f9804f
artsoroka <githubrepository@yandex.ru>
2015-03-19 10:44:55 -0400

    created mainpage template

--- app/app.js
+++ app/app.js
@@ -2,7 +2,19 @@ var express      = require('express');
 var bodyParser   = require('body-parser');
 var cookieParser = require('cookie-parser');
 var app 	 = express(); 
+var config 	 = require(__dirname +'/config'); 
 
-app.use(express.static(__dirname + '/public')); 
+app.set('config', config); 
+
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'jade'); 
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+  res.render('index', {
+    title: 'Social manager' 
+  }); 
+}); 
 
 module.exports = app;   
